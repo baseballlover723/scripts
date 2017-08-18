a5fdcb5e19d20fe563433672adb1561c23bc1501
Philip Ross <rosspa@rose-hulman.edu>
2017-08-18 01:57:20 -0700

    hamilton

--- random/renaming_songs.rb
+++ random/renaming_songs.rb
@@ -1,12 +1,16 @@
 puts "Renaming files..."
 
-folder_path = "C:/Users/Philip Ross/Music/Saijaku Muhai no Bahamut/"
+folder_path = "/mnt/c/Users/Philip Ross/Music/Hamilton (Original Broadway Cast Recording)/"
 Dir.glob(folder_path + "*").sort.each do |f|
   filename = File.basename(f, File.extname(f))
-  next unless filename.match(/\d/)
+  next unless filename.start_with?('(Disc 2)')
   puts filename
-  puts filename[5..-1]
-  File.rename(f, folder_path + filename[5..-1] + File.extname(f))
+  new_name = filename[9..-1]
+  puts new_name
+  track_num = new_name.to_i + 23
+  new_name[0..1] = track_num.to_s
+  puts new_name
+  File.rename(f, folder_path + new_name + File.extname(f))
 end
 
 puts "Renaming complete."
