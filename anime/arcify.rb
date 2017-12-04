6ee1c073ce10d205131201474b2cd7f83d4864c3
Philip Ross <dev@baseballlover723.com>
2017-12-04 00:15:09 -0800

    can now arcify shows

---
+++ anime/arcify.rb
@@ -0,0 +1,126 @@
+# requires arcifyd files
+require 'colorize'
+require 'pp'
+require 'highline/import'
+require 'fileutils'
+require 'set'
+require_relative 'arcs'
+
+PATH = '/mnt/c/Users/Philip Ross/Downloads/Naruto; Shippūden/'
+# PATH = '/mnt/d/Naruto; Shippūden/'
+# PATH = '../../raided/Naruto/'
+PATH << '/' unless PATH.end_with? '/'
+OPTS = {encoding: 'UTF-8'}
+RESULTS = []
+FILES = {}
+ARCS = ALL_ARCS[File.basename(PATH).split(/ \(/, 2).first]
+# 1.upto(500).each do |numb|
+#   File.open(PATH + numb.to_s + '.mkv', 'w')
+# end
+
+puts("Could not find directory at #{PATH}") || exit unless File.directory? PATH
+
+ARCS.keys.select {|a| a.is_a? Range}.each_cons(2) do |last, current|
+  if last.last >= current.first
+    puts "Bad Arc Order '#{current}' is after '#{last}'".light_red
+    exit
+  end
+end
+
+IGNORE_FOLDERS = [
+  'Movies',
+]
+
+# puts ARCS.pretty_inspect
+
+def main
+  puts ''
+  gather_files PATH
+
+  puts "Arcifying #{File.basename(PATH).light_cyan}"
+  if yesno(verify_arcify)
+    arcify PATH
+  end
+end
+
+def gather_files(path)
+  Dir.glob(escape_glob(path) + "**/*").sort.each do |f|
+    next unless File.file? f
+    directory_name = File.basename File.dirname f
+    next if IGNORE_FOLDERS.include? directory_name
+
+    filename = File.basename(f)
+    episode_number = filename[/\d+/].to_i
+    FILES[episode_number] = f
+  end
+end
+
+def verify_arcify
+  missing_episodes = []
+  non_arced_episodes = []
+
+  ARCS.keys.each do |episode_range|
+    next unless episode_range.is_a? Range
+    episode_range.each do |numb|
+      missing_episodes << numb unless FILES.include? numb
+    end
+  end
+  if missing_episodes.empty?
+    puts 'Found all episodes in arcs on disk'.light_green
+  else
+    puts "Can't find episodes on disk: #{missing_episodes.map(&:to_s).map(&:light_red).join(', ')}"
+  end
+
+  FILES.keys.each do |numb|
+    non_arced_episodes << numb unless ARCS[numb]
+  end
+
+  if non_arced_episodes.empty?
+    puts 'All episodes on disk have an arc'.light_green
+  else
+    puts "Can't find an arc for episodes: #{non_arced_episodes.map(&:to_s).map(&:light_red).join(', ')}"
+  end
+
+  missing_episodes.empty? && non_arced_episodes.empty?
+end
+
+def arcify(path)
+  movie_arcs = Set.new
+  ARCS.each_with_index do |(episode_range, arc_name), index|
+    folder_name = "Arc #{index + 1} (#{arc_name})/"
+    folder_name = folder_name.gsub(/ filler\)\/\z/, ') (Non-Canon)/')
+    folder_name = folder_name.gsub(/ \(\)/, '')
+    FileUtils.mkdir_p(path + folder_name)
+    movie_arcs << path + folder_name if episode_range.is_a?(String) && episode_range.start_with?('movie')
+    next unless episode_range.is_a? Range
+    episode_range.each do |episode_number|
+      file = FILES[episode_number]
+      File.rename file, path + folder_name + File.basename(file)
+    end
+  end
+  Dir.glob(escape_glob(path) + "*").each do |f|
+    next if movie_arcs.include? f + '/'
+    Dir.rmdir(f) if Dir.empty?(f)
+  end
+  puts "Done Arcifying #{File.basename(path).light_cyan}"
+end
+
+def escape_glob(s)
+  s.gsub(/[\\\{\}\[\]\*\?]/) {|x| "\\"+x}
+end
+
+def yesno(default = true, prompt = 'Continue?')
+  a = ''
+  s = default ? '[Y/n]' : '[y/N]'
+  d = default ? 'y' : 'n'
+  until %w[y n].include? a
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    a = ask("#{prompt} #{s} ") {|q| q.limit = 1; q.case = :downcase}
+    $VERBOSE = original_verbosity
+    a = d if a.length == 0
+  end
+  a == 'y'
+end
+
+main
---
+++ anime/arcs.rb
@@ -0,0 +1,161 @@
+require 'active_support/core_ext/hash/indifferent_access'
+
+class RangedHash
+  def initialize(hash)
+    @ranges = hash
+  end
+
+  def [](key)
+    @ranges.each do |range, value|
+      next unless range.is_a? Range
+      return value if range.include?(key)
+    end
+    nil
+  end
+
+  def method_missing(meth, *args, &block)
+    if @ranges.respond_to?(meth)
+      @ranges.send(meth, *args, &block)
+    else
+      super
+    end
+  end
+
+  def respond_to?(meth)
+    @ranges.respond_to?(meth)
+  end
+end
+
+ALL_ARCS = {
+  Naruto: RangedHash.new(
+    {
+      1..19 => 'Prologue; Land of Waves',
+      20..67 => 'Chūnin Exams',
+      68..80 => 'Konoha Crush',
+      81..100 => 'Search for Tsunade',
+      101..101 => ' filler',
+      'movie1' => 'Ninja Clash in the Land of Snow filler',
+      102..106 => 'Land of Tea Escort Mission filler',
+      107..135 => 'Sasuke Recovery Mission',
+      136..141 => 'Land of Rice Fields Investigation Mission filler',
+      142..147 => 'Mizuki Tracking Mission filler',
+      148..151 => 'Bikōchū Search Mission filler',
+      152..157 => 'Kurosuki Family Removal Mission filler',
+      158..160 => 'Gosunkugi Capture Mission filler',
+      'movie2' => 'Legend of the Stone of Gelel filler',
+      161..167 => 'Cursed Warrior Extermination Mission filler',
+      168..173 => 'Kaima Capture Mission filler',
+      174..176 => 'Buried Gold Excavation Mission filler',
+      177..183 => 'Star Guard Mission filler',
+      184..186 => ' filler',
+      187..191 => 'Peddlers Escort Mission filler',
+      192..194 => ' filler',
+      195..196 => 'Third Great Beast Arc filler',
+      'movie3' => 'Guardians of the Crescent Moon Kingdom filler',
+      197..201 => 'Konoha Plans Recapture Mission filler',
+      202..207 => 'Yakumo Kurama Rescue Mission filler',
+      208..212 => 'Gantetsu Escort Mission filler',
+      213..215 => 'Menma Memory Search Mission filler',
+      216..220 => 'Sunagakure Support Mission filler',
+    }
+  ),
+  'Naruto; Shippūden': RangedHash.new(
+    {
+      1..32 => 'Kazekage Rescue Mission',
+      33..53 => 'Tenchi Bridge Reconnaissance Mission',
+      54..71 => 'Twelve Guardian Ninja',
+      72..88 => 'Akatsuki Suppression Mission',
+      89..112 => "Three-Tails' Appearance",
+      113..118 => 'Itachi Pursuit Mission Pt 1',
+      119..120 => ' filler',
+      121..126 => 'Itachi Pursuit Mission Pt 2',
+      127..133 => 'Tale of Jiraiya the Gallant',
+      134..143 => 'Fated Battle Between Brothers',
+      144..151 => 'Six-Tails Unleashed',
+      152..175 => "Pain's Assault",
+      176..196 => 'Past Arc; The Locus of Konoha',
+      197..214 => 'Five Kage Summit',
+      215..222 => 'Fourth Shinobi World War; Countdown Pt 1',
+      223..242 => 'Paradise Life on a Boat',
+      243..256 => 'Fourth Shinobi World War; Countdown Pt 2',
+      257..260 => ' filler',
+      261..289 => 'Fourth Shinobi World War; Confrontation Pt 1',
+      290..295 => 'Power filler',
+      296..321 => 'Fourth Shinobi World War; Confrontation Pt 2',
+      322..348 => 'Fourth Shinobi World War; Climax Pt 1',
+      349..361 => "Kakashi's Anbu Arc; The Shinobi That Lives in the Darkness filler",
+      362..375 => 'Fourth Shinobi World War; Climax Pt 2',
+      376..377 => ' filler',
+      378..393 => "Birth of the Ten-Tails' Jinchūriki Pt 1",
+      394..413 => "In Naruto's Footsteps; The Friends' Paths filler",
+      414..427 => "Birth of the Ten-Tails' Jinchūriki Pt 2",
+      428..431 => 'Kaguya Ōtsutsuki Strikes Pt 1',
+      432..449 => 'Jiraiya Shinobi Handbook; The Tale of Naruto the Hero filler',
+      450..451 => 'Kaguya Ōtsutsuki Strikes Pt 2',
+      452..457 => 'Itachi Shinden Book; Light and Darkness filler',
+      458..468 => 'Kaguya Ōtsutsuki Strikes Pt 3',
+      469..479 => ' filler',
+      480..483 => 'Childhood filler',
+      484..488 => 'Sasuke Shinden; Book of Sunrise filler',
+      489..493 => 'Shikamaru Hiden; A Cloud Drifting in Silent Darkness filler',
+      494..500 => 'Konoha Hiden; The Perfect Day for a Wedding'
+    }
+  ),
+  Bleach: RangedHash.new(
+    {
+      1..20 => 'Agent of the Shinigami',
+      21..41 => 'The Sneak Entry',
+      42..63 => 'Soul Society; The Rescue',
+    }
+  )
+}.with_indifferent_access
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
