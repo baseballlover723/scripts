5ff008845cc441fcaf1f1043ae204885e200eb5b
Philip Ross <dev@baseballlover723.com>
2020-08-16 18:22:59 -0700

    now renames files on remote

--- anime/in_progress.rb
+++ anime/in_progress.rb
@@ -34,6 +34,10 @@ class Season < BaseSeason
   def new_name
     @name.gsub(' (In Progress)', '')
   end
+
+  def to_s
+    "#{name}: remote: #{remote_path}, local: #{local_path}, external: #{external_path} long_external: #{long_external_path}"
+  end
 end
 
 class Script < BaseScript
@@ -60,53 +64,71 @@ class Script < BaseScript
     return !season.show.name.include?('In Progress') if season.name == 'root'
     !season.name.include? 'In Progress'
   end
+
+  def rename_results
+    remote_ssh do |ssh|
+      results.each_value do |show|
+        if BaseScript.yesno("rename \"#{show.name.light_red}\": #{show.seasons.values.map(&:name).to_s.light_red}\n-> \"#{show.new_name.light_green}\": #{show.seasons.values.map(&:new_name).to_s.light_green}")
+          show.seasons.values.each do |season|
+            next if season.name == 'root'
+            rename(ssh, season)
+          end
+          rename(ssh, show)
+        end
+      end
+    end
+  end
+
+  def rename(ssh, season)
+    puts "renaming #{season.name}"
+    PATH_TYPES.each do |path_type|
+      path = season.send(path_type)
+      next unless path
+      new_path = File.dirname(path) + '/' + season.new_name
+      path_type.to_s.start_with?('remote') ? remote_rename(ssh, path, new_path) : local_rename(path, new_path)
+    end
+  end
+
+  def local_rename(old_path, new_path)
+    File.rename old_path, new_path
+  end
+
+  def remote_rename(ssh, old_path, new_path)
+    ssh.exec!("mv '#{old_path}' '#{new_path}'")
+  end
 end
 
 def main
   script = Script.new
   # script.location = 'local' # debug
-  # script.iterate '/mnt/d/anime'
+  # # script.iterate '/mnt/d/anime'
   # script.iterate '/mnt/c/Users/Philip Ross/Downloads/anime'
-  script.remotely_iterate("/entertainment/anime")
+  # # script.remotely_iterate("/entertainment/anime")
+  # script.remotely_iterate("/home/baseballlover723/remote_test/anime")
+  # puts script.results.size
+  #
+  # script.remotely_iterate("/home/baseballlover723/remote_test/anime")
   # script.location = 'local' # debug
   # script.iterate '/mnt/c/Users/Philip Ross/Downloads/anime'
   # script.location = 'external' # debug
   # script.iterate '/mnt/c/Users/Philip Ross/Downloads/anime external'
-  # script.iterate '/mnt/d/anime'
-  # script.iterate '/mnt/g/anime'
-  # script.iterate '/mnt/f/anime'
 
-  # puts script.results
+  script.remotely_iterate("/entertainment/anime")
+  script.iterate '/mnt/d/anime'
+  script.iterate '/mnt/g/anime'
+  script.iterate '/mnt/f/anime'
+
   script.trim_results
   script.end_time
-  results = script.results
 
-  puts "\n\n" # debug
+  # puts "\n\n" # debug
 
-  results.each_value do |show|
-    if BaseScript.yesno("rename \"#{show.name.light_red}\": #{show.seasons.values.map(&:name).to_s.light_red}\n-> \"#{show.new_name.light_green}\": #{show.seasons.values.map(&:new_name).to_s.light_green}")
-      show.seasons.values.each do |season|
-        next if season.name == 'root'
-        rename(season)
-      end
-      rename(show)
-    end
-  end
+  script.rename_results
 
-  puts "\n\n" # debug
-  puts results.map { |_, r| r.name } # debug
-  puts results.size # debug
-end
-
-def rename(season)
-  puts "renaming #{season.name}"
-  PATH_TYPES.each do |path_type|
-    path = season.send(path_type)
-    next unless path
-    base = File.dirname path
-    # puts "#{path} -> #{base + '/' + season.new_name}" # debug
-    File.rename path, base + '/' + season.new_name
-  end
+  # puts "\n\n" # debug
+  # puts results.map { |_, r| r.name } # debug
+  # puts results.size # debug
+  # puts results.values.first.seasons.values.first#.episodes.values.first
 end
 
 if ARGV.empty?
