cda454d611b11a7c22f99fb627b8762c061afb01
Philip Ross <dev@baseballlover723.com>
2021-04-13 22:14:46 -0700

    completely revamped rsync to be able to specify just 1, or from a bunch of files. It will now iterate with multiple runs to ensure that everything is synced correctly

--- anime/rsync.rb
+++ anime/rsync.rb
@@ -1,66 +1,153 @@
 require 'dotenv/load'
 require 'pty'
 require 'shellwords'
+require 'active_support/core_ext/string/indent'
+$indent_size = 4
+$indent = -$indent_size
 
 LOCAL_PATH = '/mnt/d/anime'
-# LOCAL_PATH = '/mnt/d/anime/zWatched'
 REMOTE_PATH = '/entertainment/anime'
 # LOCAL_PATH = '/mnt/e/movies'
 # REMOTE_PATH = '/entertainment/movies'
-ANIME_NAME = ".hack"
+# ANIME_NAME = ".hack"
 # ANIME_NAME = 'Olympus Has Fallen/Angel Has Fallen (2019) [1080p] {x265}'
-# ANIME_NAME = 'Log Horizon'
 OPTS = {encoding: 'UTF-8'}
 
-MODE = :sync_with_new_mtimes
-# MODE = :sync_file_with_mtimes
-# MODE = :sync_directories_with_mtimes
+MODES = [:unstarted, :sync_with_new_mtimes, :sync_file_with_mtimes, :sync_directories_with_mtimes, :done]
 
-case MODE
-when :sync_with_new_mtimes
-  OPTIONS = "-chPv -e 'ssh -p 666' --timeout 10 --protect-args"
-  DIRECTORIES = false
-when :sync_file_with_mtimes
-  OPTIONS = "-ahPv -e 'ssh -p 666' --timeout 10 --protect-args"
-  DIRECTORIES = false
-when :sync_directories_with_mtimes
-  OPTIONS = "-ahPv -e 'ssh -p 666' --include='*/' --exclude='*' --timeout 10 --protect-args" # directory
-  DIRECTORIES = true
-end
 # -a archieve
 # -c use checksums instead of file size
 # -h human readable numbers
 # -P progress bar
 # -v verbose
-
-def main
-  iterate_recursive local do |episode_path|
-    local_path = "#{local}#{episode_path}".shellescape
-    episode_path = File.dirname(episode_path)[0..-2] if DIRECTORIES # send to parent and remove . from end
-    remote_path = "#{remote}#{episode_path}".shellescape
-    puts "syncing #{local_path} to #{remote_path}"
-    run_shell_command "rsync #{options} #{local_path} #{remote_path}"
+def calc_options(mode)
+  case mode
+  when :sync_with_new_mtimes
+    options = "-chPv -e 'ssh -p 666' --timeout 10 --protect-args"
+    directories = false
+    return options, directories
+  when :sync_file_with_mtimes
+    options = "-ahPv -e 'ssh -p 666' --timeout 10 --protect-args"
+    directories = false
+    return options, directories
+  when :sync_directories_with_mtimes
+    options = "-ahPv -e 'ssh -p 666' --include='*/' --exclude='*' --timeout 10 --protect-args" # directory
+    directories = true
+    return options, directories
   end
 end
 
-def options
-  OPTIONS
+def main
+  if defined? ANIME_NAME
+    rsync_animes([ANIME_NAME])
+  else
+    iterate_anime_files("dirty_animes_*.txt")
+  end
 end
 
-def local
-  "#{LOCAL_PATH}/#{ANIME_NAME}/"
+def move_mode_forwards(path)
+  lines = File.read(path).split("\n")
+  mode = lines.shift.to_sym
+  next_mode = calc_next_mode(mode)
+  content = "#{next_mode.to_s}\n"
+  content += lines.join("\n")
+  content += "\n"
+  File.write(path, content)
+  next_mode
 end
 
-def remote
-  "#{ENV['OVERMIND_USER']}@#{ENV['OVERMIND_HOST']}:#{REMOTE_PATH}/#{ANIME_NAME}/"
+def calc_next_mode(mode)
+  MODES.each_cons(2) do |current_mode, next_mode|
+    return next_mode if current_mode == mode
+  end
+  MODES.last
 end
 
-def iterate_recursive(path)
-  Dir.glob(escape_glob(path) + '**/*').reject { |f| DIRECTORIES ^ File.directory?(f) }.each do |f|
+def local(anime)
+  path = "#{LOCAL_PATH}/#{anime}/"
+  path = "#{LOCAL_PATH}/zWatched/#{anime}/" unless Dir.exist?(path)
+  raise "can't find anime #{anime} at \"#{"#{LOCAL_PATH}/#{anime}/"}\" or \"#{path}\"" unless Dir.exist?(path)
+  path
+end
+
+def remote(anime)
+  "#{ENV['OVERMIND_USER']}@#{ENV['OVERMIND_HOST']}:#{REMOTE_PATH}/#{anime}/"
+end
+
+def iterate_anime_files(glob)
+  entries = Dir.glob glob
+  entries.each.with_index do |path, i|
+    iterate_anime_file(path, " (#{i + 1} / #{entries.size})")
+  end
+end
+
+def iterate_anime_file(path, index_str)
+  $indent += $indent_size
+  animes = File.read(path).split("\n")
+  mode = animes.shift.to_sym
+  $indent -= $indent_size and return if mode == :done
+
+  puts "\n**************************************\n\nanime file: #{path}#{index_str} &&&&&&&&&&&&&&&&&&&&&&&&".indent $indent
+  rsync_animes(animes, mode, path)
+end
+
+def rsync_animes(animes, mode = MODES.first, path = nil)
+  if mode == MODES.first
+    mode = (path ? move_mode_forwards(path) : calc_next_mode(mode))
+  end
+
+  while mode != MODES.last
+    animes.each.with_index do |anime, i|
+      rsync_anime(anime, mode, " (#{i + 1} / #{animes.size})")
+    end
+    mode = (path ? move_mode_forwards(path) : calc_next_mode(mode))
+    print "\a"
+  end
+  $indent -= $indent_size
+end
+
+def rsync_anime(anime, mode, index_str = '')
+  $indent += $indent_size
+  puts "\n Rsyncing #{anime} with mode #{mode}#{index_str} ******************\n\n".indent $indent, "*"
+  options, directories = calc_options(mode)
+  local_anime = local(anime)
+
+  $indent += $indent_size
+  iterate_recursive local_anime, directories do |episode_path|
+    local_path = "#{local_anime}#{episode_path}"
+    episode_path = File.dirname(episode_path)[0..-2] if directories # send to parent and remove . from end
+    remote_path = "#{remote(anime)}#{episode_path}"
+    puts "syncing #{local_path} to #{remote_path}".indent $indent
+    run_shell_command "rsync #{options} #{local_path.shellescape} #{remote_path.shellescape}"
+  end
+  if directories
+    # Root folder
+    local_path = local_anime[0..-2]
+    remote_path = File.dirname(remote(anime))
+    puts "syncing #{local_path} to #{remote_path}".indent $indent
+    run_shell_command "rsync #{options} #{local_path.shellescape} #{remote_path.shellescape}"
+  end
+  $indent -= $indent_size
+
+  $indent -= $indent_size
+end
+
+def local(anime)
+  path = "#{LOCAL_PATH}/#{anime}/"
+  path = "#{LOCAL_PATH}/zWatched/#{anime}/" unless Dir.exist?(path)
+  raise "can't find anime #{anime} at \"#{"#{LOCAL_PATH}/#{anime}/"}\" or \"#{path}\"" unless Dir.exist?(path)
+  path
+end
+
+def remote(anime)
+  "#{ENV['OVERMIND_USER']}@#{ENV['OVERMIND_HOST']}:#{REMOTE_PATH}/#{anime}/"
+end
+
+def iterate_recursive(path, directories = false)
+  Dir.glob(escape_glob(path) + '**/*').reject { |f| directories ^ File.directory?(f) }.each do |f|
     relative_path = f.sub path, ''
     yield relative_path
   end
-
 end
 
 def run_shell_command(command)
