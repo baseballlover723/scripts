7793537394d0a11b4d73165ae608ce6c915f14cd
Philip Ross <dev@baseballlover723.com>
2021-04-13 17:13:50 -0700

    added modes to rsync

--- anime/rsync.rb
+++ anime/rsync.rb
@@ -12,12 +12,33 @@ ANIME_NAME = ".hack"
 # ANIME_NAME = 'Log Horizon'
 OPTS = {encoding: 'UTF-8'}
 
-OPTIONS = "-acPv -e 'ssh -p 666' --timeout 10 --protect-args"
+MODE = :sync_with_new_mtimes
+# MODE = :sync_file_with_mtimes
+# MODE = :sync_directories_with_mtimes
+
+case MODE
+when :sync_with_new_mtimes
+  OPTIONS = "-chPv -e 'ssh -p 666' --timeout 10 --protect-args"
+  DIRECTORIES = false
+when :sync_file_with_mtimes
+  OPTIONS = "-ahPv -e 'ssh -p 666' --timeout 10 --protect-args"
+  DIRECTORIES = false
+when :sync_directories_with_mtimes
+  OPTIONS = "-ahPv -e 'ssh -p 666' --include='*/' --exclude='*' --timeout 10 --protect-args" # directory
+  DIRECTORIES = true
+end
+# -a archieve
+# -c use checksums instead of file size
+# -h human readable numbers
+# -P progress bar
+# -v verbose
 
 def main
   iterate_recursive local do |episode_path|
     local_path = "#{local}#{episode_path}".shellescape
+    episode_path = File.dirname(episode_path)[0..-2] if DIRECTORIES # send to parent and remove . from end
     remote_path = "#{remote}#{episode_path}".shellescape
+    puts "syncing #{local_path} to #{remote_path}"
     run_shell_command "rsync #{options} #{local_path} #{remote_path}"
   end
 end
@@ -35,10 +56,9 @@ def remote
 end
 
 def iterate_recursive(path)
-  Dir.glob(escape_glob(path) + '**/*').reject {|f| File.directory? f}.each do |f|
+  Dir.glob(escape_glob(path) + '**/*').reject { |f| DIRECTORIES ^ File.directory?(f) }.each do |f|
     relative_path = f.sub path, ''
     yield relative_path
-    # break
   end
 
 end
@@ -47,7 +67,7 @@ def run_shell_command(command)
   begin
     PTY.spawn(command) do |stdout, stdin, pid|
       begin
-        stdout.each_char {|line| print line}
+        stdout.each_char { |line| print line }
         stdout.flush
       rescue Errno::EIO
       end
@@ -57,7 +77,7 @@ def run_shell_command(command)
 end
 
 def escape_glob(s)
-  s.gsub(/[\\\{\}\[\]\*\?]/) {|x| "\\" + x}
+  s.gsub(/[\\\{\}\[\]\*\?]/) { |x| "\\" + x }
 end
 
 main
