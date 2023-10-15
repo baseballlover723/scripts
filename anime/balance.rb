c08d77467f121300c2a2678323d006bb4c78f684
Philip Ross <dev@baseballlover723.com>
2023-10-15 15:18:12 -0700

    update balance to not require input

--- anime/balance.rb
+++ anime/balance.rb
@@ -1,34 +1,49 @@
-require 'optparse'
+require 'sys/filesystem'
+require 'active_support'
+require 'active_support/number_helper'
 
-# CAP_ANIME = 3.63 # TB
-# CAP_LONG = 1.81 # TB
-CAP_ANIME = 4.54 # TB
-CAP_LONG = 3.63 # TB
-CAP_TOTAL = CAP_ANIME + CAP_LONG
-RATIO_ANIME = CAP_ANIME / CAP_TOTAL
-RATIO_LONG = CAP_LONG / CAP_TOTAL
+ANIME_PATH = "/mnt/g"
+LONG_PATH = "/mnt/f"
+
+def main
+  stat_anime = Sys::Filesystem.stat(ANIME_PATH)
+  stat_long = Sys::Filesystem.stat(LONG_PATH)
+
+  cap_anime = total(stat_anime)
+  cap_long = total(stat_long)
+  cap_total = cap_anime + cap_long
+  ratio_anime = cap_anime / cap_total
+  ratio_long = cap_long / cap_total
+
+  free_anime = free(stat_anime)
+  free_long = free(stat_long)
+  free_total = free_anime + free_long
+
+  ideal_free_anime = free_total * ratio_anime
+  ideal_free_long = free_total * ratio_long
 
 
-help = "Usage: balance.rb [free anime space in GB] [free long anime space in GB]\nex: balance.rb 20.5 15.8"
-if ARGV.size != 2
-  puts "invalid amount of arguments, please pass 2 arguments"
-  puts help
+  puts "ideal_free_anime: #{human_size(ideal_free_anime)}"
+  puts "ideal_free_long: #{human_size(ideal_free_long)}"
+
+  move = ideal_free_anime - free_anime
+  if move > 0
+    puts "move #{human_size(move)} from anime to long"
+  else
+    puts "move #{human_size(-move)} from long to anime"
+  end
 end
 
-free_anime = ARGV[0].to_f
-free_long = ARGV[1].to_f
-free_total = free_anime + free_long
-
-ideal_free_anime = free_total * RATIO_ANIME
-ideal_free_long = free_total * RATIO_LONG
-
-puts "ideal_free_anime: #{ideal_free_anime}"
-puts "ideal_free_long: #{ideal_free_long}"
-
-move = ideal_free_anime - free_anime
-if move > 0
-  puts "move #{move} GB from anime to long"
-else
-  puts "move #{-move} GB from long to anime"
+def total(stat)
+  stat.block_size * stat.blocks.to_f
 end
 
+def free(stat)
+  stat.block_size * stat.blocks_available.to_f
+end
+
+def human_size(size)
+  ActiveSupport::NumberHelper.number_to_human_size(size, {precision: 5, strip_insignificant_zeros: false})
+end
+
+main
